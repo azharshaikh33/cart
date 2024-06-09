37a41630c535b349a0b728cced630870204da829
Azhar <azharshaikh33@gmail.com>
2024-06-09 17:18:38 +0400

    Auto Push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -4,6 +4,6 @@ module "app" {
     ENV                 = var.ENV
     INSTANCE_TYPE       = var.INSTANCE_TYPE
     SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
-    OD_INSTANCE_TYPE    = var.OD_INSTANCE_TYPE
+    OD_INSTANCE_TYPE    = var.OD_INSTANCE_COUNT
     APP_PORT            = var.APP_PORT
 }
