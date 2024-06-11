6e00d3d07adc5690650d559875c07788bed13130
Azhar <azharshaikh33@gmail.com>
2024-06-11 08:08:50 +0400

    Auto Push

--- mutable-infra/main.tf
+++ mutable-infra/main.tf
@@ -1,9 +1,9 @@
-module "cart" {
+module "app" {
     source              = "./vendor/modules/app"
     COMPONENT           = var.COMPONENT
     ENV                 = var.ENV
     INSTANCE_TYPE       = var.INSTANCE_TYPE
     SPOT_INSTANCE_COUNT = var.SPOT_INSTANCE_COUNT
-    OD_INSTANCE_TYPE    = var.OD_INSTANCE_COUNT
+    OD_INSTANCE_COUNT   = var.OD_INSTANCE_COUNT
     APP_PORT            = var.APP_PORT
 }
